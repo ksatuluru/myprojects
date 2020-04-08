facba374b0a128f927622c8416babac5a07a398a
kamalsatuluru <kamalsatuluru@kamals-mbp>
2020-04-08 15:11:16 -0400

    PureJava - Release1

---
+++ PureJavaProject/src/SimpleJava.java
@@ -0,0 +1,14 @@
+
+public class SimpleJava {
+	
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main (String args[])
+	{
+		new SimpleJava().display();
+	}
+
+}
